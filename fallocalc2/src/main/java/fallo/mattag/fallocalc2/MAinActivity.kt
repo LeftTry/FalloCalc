e2f04d6bf6d11053ed1a1304c95075513b864139
LeftTry <50211739+LeftTry@users.noreply.github.com>
2019-10-08 18:08:24 +0300

    1.0.0

---
+++ fallocalc2/src/main/java/fallo/mattag/fallocalc2/MAinActivity.kt
@@ -0,0 +1,12 @@
+package fallo.mattag.fallocalc2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MAinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
