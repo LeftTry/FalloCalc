5379d7eb73e7f93daebe74301a131390ed41303b
LeftTry <50211739+LeftTry@users.noreply.github.com>
2019-10-17 21:07:31 +0300

    1.0.0

--- app/src/main/java/fallo/fallocalc/MainActivity.kt
+++ app/src/main/java/fallo/fallocalc/MainActivity.kt
@@ -1,19 +1,18 @@
 package fallo.fallocalc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Double
-import java.lang.Integer
 
 
 @Suppress("UNUSED_CHANGED_VALUE", "DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-
-    private val TAG                         = "FalloCalc"
+//  initialization of variables
+    private val tag                         = "FalloCalc"
     private var plusClicked                 = false
     private var minusClicked                = false
     private var multipleClicked             = false
@@ -27,9 +26,7 @@ class MainActivity : AppCompatActivity() {
     private var someClicked                 = false
     private var firstPointClicked           = false
     private var secondPointClicked          = false
-    private var firstComponentInt : Long    = 0
-    private var secondComponentInt : Long   = 0
-    private var answerInt : Long            = 0
+    private var stringAnswer                = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,34 +34,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onZeroClick(view: View) {
+//        onClick function for zero button
         var text = inputView.text.toString()
         text += "0"
+//        inputView - TextView for input expression
         inputView.text = text
         if (isFirstComponent) {
             if (someClicked) {
                 nowSecondAnswer = "0"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 0")
+                Log.d(tag, "sa = 0")
             }
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 0
-                Log.d(TAG, "fa += 0")
+                Log.d(tag, "fa += 0")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "0"
-                Log.d(TAG, "fa = 0")
+                Log.d(tag, "fa = 0")
             }
 
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 0
-                Log.d(TAG, "sa += 0")
+                Log.d(tag, "sa += 0")
             }
         }
     }
 
     fun onPointClick(view: View) {
+//        onClick function for point button
+//        checking for the first input
         if (isFirstComponent) {
             if(nowAnswer != "0" && !someClicked && !firstPointClicked){
                 var text : String  = inputView.text.toString()
@@ -72,7 +73,7 @@ class MainActivity : AppCompatActivity() {
                 inputView.text     = text
                 firstPointClicked  = true
                 nowAnswer          += "."
-                Log.d(TAG, "fa += .")
+                Log.d(tag, "fa += .")
             }
         }
         else{
@@ -82,82 +83,57 @@ class MainActivity : AppCompatActivity() {
                 inputView.text      = text
                 nowSecondAnswer     += "."
                 secondPointClicked  = true
-                Log.d(TAG, "sa += .")
+                Log.d(tag, "sa += .")
         }
         }
     }
 
     fun onEqualsClick(view: View) {
-            firstComponentDouble        = Double.parseDouble(nowAnswer)
-            secondComponentDouble       = Double.parseDouble(nowSecondAnswer)
-            deleteButton.visibility     = View.INVISIBLE
-            allDeleteButton.visibility  = View.VISIBLE
-
-            if (plusClicked)      answer = firstComponentDouble + secondComponentDouble
-
-            if (minusClicked)     answer = firstComponentDouble - secondComponentDouble
-
-            if (multipleClicked)  answer = firstComponentDouble * secondComponentDouble
-
-            if (divideClicked)    answer = firstComponentDouble / secondComponentDouble
-
-            Log.d(
-                TAG,
-                "fc: $firstComponentDouble sc: $secondComponentDouble fa: $nowAnswer sa: $nowSecondAnswer a: $answer"
-            )
-
-            inputView.text         = answer.toString()
-            divideClicked          = false
-            multipleClicked        = false
-            minusClicked           = false
-            plusClicked            = false
-            someClicked            = false
-            nowAnswer              = answer.toString()
-            nowSecondAnswer        = "0"
-            isFirstComponent       = true
-            secondComponentDouble  = 0.0
-            firstPointClicked      = false
-            secondPointClicked     = false
+//        onClick function for equals button
+        sum()
     }
 
     fun onPlusClick(view: View) {
+        //onClick function for plus button
         var text = inputView.text.toString()
         text += "+"
         inputView.text = text
         plusClicked = true
-        Log.d(TAG, "plusClicked = true")
+        Log.d(tag, "plusClicked = true")
         someClicked = true
     }
 
     fun onOneClick(view: View) {
+//        onClick function for one button
         var text = inputView.text.toString()
         text += "1"
         inputView.text = text
         if (isFirstComponent) {
             if (someClicked) {
-                Log.d(TAG, "sa = 1")
+                Log.d(tag, "sa = 1")
                 nowSecondAnswer = "1"
                 isFirstComponent = false
             }
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 1
-                Log.d(TAG, "fa += 1")
+                Log.d(tag, "fa += 1")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "1"
-                Log.d(TAG, "fa = 1")
+                Log.d(tag, "fa = 1")
             }
 
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 1
-                Log.d(TAG, "sa += 1")
+                Log.d(tag, "sa += 1")
             }
         }
     }
 
     fun onTwoClick(view: View) {
+//        onClick function for two button
         var text = inputView.text.toString()
         text += "2"
         inputView.text = text
@@ -165,27 +141,28 @@ class MainActivity : AppCompatActivity() {
 
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 2
-                Log.d(TAG, "fa += 2")
+                Log.d(tag, "fa += 2")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "2"
-                Log.d(TAG, "fa = 1")
+                Log.d(tag, "fa = 1")
             }
             if (someClicked) {
                 nowSecondAnswer = "2"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 2")
+                Log.d(tag, "sa = 2")
             }
         }
         else {
             if (someClicked) {
                 nowSecondAnswer += 2
-                Log.d(TAG, "sa += 2")
+                Log.d(tag, "sa += 2")
             }
         }
     }
 
     fun onThreeClick(view: View) {
+//        onClick function for three button
         var text = inputView.text.toString()
         text += "3"
         inputView.text = text
@@ -193,27 +170,28 @@ class MainActivity : AppCompatActivity() {
             if (someClicked) {
                 nowSecondAnswer = "3"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 3")
+                Log.d(tag, "sa = 3")
             }
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 3
-                Log.d(TAG, "fa += 3")
+                Log.d(tag, "fa += 3")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "3"
-                Log.d(TAG, "fa = 3")
+                Log.d(tag, "fa = 3")
             }
 
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 3
-                Log.d(TAG, "sa += 3")
+                Log.d(tag, "sa += 3")
             }
         }
     }
 
     fun onMultipleClick(view: View) {
+//        onClick function for multiple button
         var text = inputView.text.toString()
         text += "*"
         inputView.text = text
@@ -222,6 +200,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onFourClick(view: View) {
+//        onClick function for four button
         var text = inputView.text.toString()
         text += "4"
         inputView.text = text
@@ -229,26 +208,27 @@ class MainActivity : AppCompatActivity() {
             if (someClicked) {
                 nowSecondAnswer = "4"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 4")
+                Log.d(tag, "sa = 4")
             }
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 4
-                Log.d(TAG, "fa += 4")
+                Log.d(tag, "fa += 4")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "4"
-                Log.d(TAG, "fa = 4")
+                Log.d(tag, "fa = 4")
             }
         }
         else{
             if (someClicked) {
-                Log.d(TAG, "sa += 4")
+                Log.d(tag, "sa += 4")
                 nowSecondAnswer += 4
             }
         }
     }
 
     fun onFiveClick(view: View) {
+//        onClick function for five button
         var text = inputView.text.toString()
         text += "5"
         inputView.text = text
@@ -256,27 +236,28 @@ class MainActivity : AppCompatActivity() {
             if (someClicked) {
                 nowSecondAnswer = "5"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 5")
+                Log.d(tag, "sa = 5")
             }
             if(nowAnswer != "0" && !someClicked){
-                Log.d(TAG, "fa += 5")
+                Log.d(tag, "fa += 5")
                 nowAnswer += 5
             }
             if (nowAnswer == "0") {
                 nowAnswer = "5"
-                Log.d(TAG, "fa = 5")
+                Log.d(tag, "fa = 5")
             }
 
         }
         else{
             if (someClicked) {
-                Log.d(TAG, "sa += 5")
+                Log.d(tag, "sa += 5")
                 nowSecondAnswer += 5
             }
         }
     }
 
     fun onSixClick(view: View) {
+//        onClick function for six button
         var text = inputView.text.toString()
         text += "6"
         inputView.text = text
@@ -284,26 +265,27 @@ class MainActivity : AppCompatActivity() {
             if (someClicked) {
                 nowSecondAnswer = "6"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 6")
+                Log.d(tag, "sa = 6")
             }
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 6
-                Log.d(TAG, "fa += 6")
+                Log.d(tag, "fa += 6")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "6"
-                Log.d(TAG, "fa = 6")
+                Log.d(tag, "fa = 6")
             }
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 6
-                Log.d(TAG, "sa += 6")
+                Log.d(tag, "sa += 6")
             }
         }
     }
 
     fun onMinusClick(view: View) {
+//        onClick function for minus button
         var text = inputView.text.toString()
         text += "-"
         inputView.text = text
@@ -312,33 +294,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onSevenClick(view: View) {
+//        onClick function for seven button
         var text = inputView.text.toString()
         text += "7"
         inputView.text = text
         if (isFirstComponent) {
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 7
-                Log.d(TAG, "fa += 7")
+                Log.d(tag, "fa += 7")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "7"
-                Log.d(TAG, "fa = 7")
+                Log.d(tag, "fa = 7")
             }
             if (someClicked) {
                 nowSecondAnswer = "7"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 7")
+                Log.d(tag, "sa = 7")
             }
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 7
-                Log.d(TAG, "sa += 7")
+                Log.d(tag, "sa += 7")
             }
         }
     }
 
     fun onEightClick(view: View) {
+//        onClick function for eight button
         var text = inputView.text.toString()
         text += "8"
         inputView.text = text
@@ -346,28 +330,29 @@ class MainActivity : AppCompatActivity() {
 
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 8
-                Log.d(TAG, "fa += 8")
+                Log.d(tag, "fa += 8")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "8"
-                Log.d(TAG, "fa = 8")
+                Log.d(tag, "fa = 8")
             }
 
             if (someClicked) {
                 nowSecondAnswer = "8"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 8")
+                Log.d(tag, "sa = 8")
             }
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 8
-                Log.d(TAG, "sa += 8")
+                Log.d(tag, "sa += 8")
             }
         }
     }
 
     fun onNineClick(view: View) {
+//        onClick function for nine button
         var text = inputView.text.toString()
         text += "9"
         inputView.text = text
@@ -375,27 +360,28 @@ class MainActivity : AppCompatActivity() {
             if (someClicked) {
                 nowSecondAnswer = "9"
                 isFirstComponent = false
-                Log.d(TAG, "sa = 9")
+                Log.d(tag, "sa = 9")
             }
             if(nowAnswer != "0" && !someClicked){
                 nowAnswer += 9
-                Log.d(TAG, "fa += 9")
+                Log.d(tag, "fa += 9")
             }
             if (nowAnswer == "0") {
                 nowAnswer = "9"
-                Log.d(TAG, "fa = 9")
+                Log.d(tag, "fa = 9")
             }
 
         }
         else{
             if (someClicked) {
                 nowSecondAnswer += 9
-                Log.d(TAG, "sa += 9")
+                Log.d(tag, "sa += 9")
             }
         }
     }
 
     fun onDivideClick(view: View) {
+//        onClick function for divide button
         var text = inputView.text.toString()
         text += "/"
         inputView.text = text
@@ -405,6 +391,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDeleteClick(view: View) {
+        //onClick function for delete button
         var text = inputView.text.toString()
         text = text.dropLast(1)
         inputView.text = text
@@ -417,11 +404,51 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onAllDeleteClick(view: View) {
+//        onClick function for allDelete button
         deleteButton.visibility = View.VISIBLE
         view.visibility = View.INVISIBLE
         nowAnswer = "0"
-        Log.d(TAG, "nowAnswer = $nowAnswer")
+        Log.d(tag, "nowAnswer = $nowAnswer")
         inputView.text = ""
+    }
+
+    private fun sum(){
+//      function for sum of to numbers
+        firstComponentDouble        = Double.parseDouble(nowAnswer)
+        secondComponentDouble       = Double.parseDouble(nowSecondAnswer)
+//        set button for deleting all text
+        deleteButton.visibility     = View.INVISIBLE
+        allDeleteButton.visibility  = View.VISIBLE
+
+        if (plusClicked)      answer = firstComponentDouble + secondComponentDouble
+
+        if (minusClicked)     answer = firstComponentDouble - secondComponentDouble
+
+        if (multipleClicked)  answer = firstComponentDouble * secondComponentDouble
+
+        if (divideClicked)    answer = firstComponentDouble / secondComponentDouble
+
+        Log.d(
+            tag,
+            "fc: $firstComponentDouble sc: $secondComponentDouble fa: $nowAnswer sa: $nowSecondAnswer a: $answer"
+        )
+
+        stringAnswer           = answer.toString()
+        if (stringAnswer.drop(stringAnswer.length - 2) == ".0")
+            stringAnswer = stringAnswer.dropLast(2)
+
+        inputView.text         = stringAnswer
+        divideClicked          = false
+        multipleClicked        = false
+        minusClicked           = false
+        plusClicked            = false
+        someClicked            = false
+        nowAnswer              = answer.toString()
+        nowSecondAnswer        = "0"
+        isFirstComponent       = true
+        secondComponentDouble  = 0.0
+        firstPointClicked      = false
+        secondPointClicked     = false
     }
 }
 
